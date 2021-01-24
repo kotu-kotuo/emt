6e07c3d9d2b828f1d914c11b6533c4aff55689cf
kotu-kotuo <ttn-0223@mineo.jp>
2021-01-25 02:20:50 +0900

    テスト完成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,9 @@
 FactoryBot.define do
+
   factory :user do
     email { Faker::Internet.email }
     password { 'password' }
     username { Faker::Lorem.characters(number: 7) }
   end
 
-  trait :with_profile do
-    after :build do |user|
-        build(:profile, user: user)
-    end
-  end
 end
---
+++ spec/system/article_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Article', type: :system do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
+  it '記事一覧が表示される' do
+    visit root_path
+
+      expect(page).to have_content(user.username)
+    
+  end
+
+end
